8d033c5f3610ec7702f4429cd0e11b716d9b177e
Your Name <20110568@student.hcmute.edu.vn>
2022-10-30 22:19:34 +0700

    sdd

--- AuroraMySQL.py
+++ AuroraMySQL.py
@@ -1,7 +1,7 @@
 import insertUtil as ut
 import MySQLdb
 
-con = MySQLdb.connect(host='database-aura.cluster-cn4gbnrdol43.us-east-1.rds.amazonaws.com', user ='admin', passwd = 'thang1610', port = '3306')
+con = MySQLdb.connect(host='database-aura.cluster-cn4gbnrdol43.us-east-1.rds.amazonaws.com', user ='admin', passwd = 'thang1610',db = 'aurora', port = '3306')
 print('Connect Successfully');
 ut.insert(con);
 ut.select(con);
